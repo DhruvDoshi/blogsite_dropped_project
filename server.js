39f965c161df1d95820a17ee4d18a21641b0a387
DhruvDoshi <dhruvdoshi25071999@gmail.com>
2020-05-17 19:55:49 +0530

    setting view engine up

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const app = express()
 
+
+//setting up view engine -- coverrt ejs code to html
+app.set('view engine', 'ejs')
+
+
 app.get('/', (req, res) => {
     res.send('Hello World')
 })

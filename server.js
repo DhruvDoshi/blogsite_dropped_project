3fd95b3eab97f70a97c5927fa807eb73ca727601
DhruvDoshi <dhruvdoshi25071999@gmail.com>
2020-05-17 23:00:43 +0530

    mongo db cloud

--- server.js
+++ server.js
@@ -5,9 +5,20 @@ const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
 
-mongoose.connect('mongodb://localhost/blog', {
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-})
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://dhruv:dhruv25071999@cluster0-szusn.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true 
+  });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))

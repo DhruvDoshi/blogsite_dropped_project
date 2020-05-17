2a4f1322b1e72710cac11ddb4ffd9e728802b49e
DhruvDoshi <dhruvdoshi25071999@gmail.com>
2020-05-17 20:28:25 +0530

    added render system

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express")
+const articleRouter = require('./routes/articles')
 const app = express()
-
 
 //setting up view engine -- coverrt ejs code to html
 app.set('view engine', 'ejs')
 
+app.use('/articles', articleRouter)
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    const articles = [{
+        tittle: "Text Article",
+        createdAt: Date.now(),
+        description: 'Test Description'
+    }]
+    res.render('index.ejs', { articles: articles})
 })
 
 
